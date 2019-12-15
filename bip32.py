893901bd3ef6e0eed236e1e2ce980da7be9c896a
firepond <915644848@qq.com>
2019-12-15 16:35:21 +0800

    BIP32 functions

--- bip32.py
+++ bip32.py
@@ -1,26 +1,50 @@
 import hashlib
 import hmac
 import struct
+import ecdsa
+
 from secp256k1 import PrivateKey
 from binascii import hexlify, unhexlify
+from ecdsa.ecdsa import int_to_string, string_to_int
+from ecdsa.curves import SECP256k1
+from ecdsa.numbertheory import square_root_mod_prime as sqrt_mod
+
+MIN_ENTROPY_LEN = 128        # bits
+BIP32_HARDEN    = 0x80000000 # choose from hardened set of child keys
+CURVE_GEN       = ecdsa.ecdsa.generator_secp256k1
+CURVE_ORDER     = CURVE_GEN.order()
+FIELD_ORDER     = SECP256k1.curve.p()
+INFINITY        = ecdsa.ellipticcurve.INFINITY
 
 
 def privateToPublic(privateKey):
-    privkey = PrivateKey(unhexlify(privateKey))
-    compressed = hexlify(privkey.pubkey.serialize()).decode('ascii')
-    # uncompressed = hexlify(privkey.pubkey.serialize(compressed=False)).decode('ascii')
-    # print(compressed)
-    # print(uncompressed)
-    return compressed
+  if(type(privateKey) == str):
+    privateKey = unhexlify(privateKey)
+  privkey = PrivateKey(privateKey)
+  compressed = privkey.pubkey.serialize()
+  # uncompressed = hexlify(privkey.pubkey.serialize(compressed=False)).decode('ascii')
+  # print(compressed)
+  # print(uncompressed)
+  return compressed
 
 
-def childKeyDerivation(parentPriavteKey, parnetChaincode, index):
-    BIP32_HARDEN    = 0x80000000
-
-    i_str = struct.pack(">L", index)
-    if index & BIP32_HARDEN:
-        data = bytes(0) + parentPriavteKey + i_str
-    else:
+def childKeyDerivationPrivate(parentPriavteKey, parnetChaincode, index):
+  
+  i_str = ser32(index)
+  if index & BIP32_HARDEN:
+    data = bytes.fromhex("00") + parentPriavteKey + bytes(i_str)
+  else:
+    data = privateToPublic(parentPriavteKey) + bytes(i_str)
+  (Il, Ir) = hmacsha512(data, parnetChaincode)
+  ilInt =  string_to_int(Il)
+  if ilInt > CURVE_ORDER:
+    return None
+  privateInt = string_to_int(parentPriavteKey)
+  priavteKeyInt = (ilInt + privateInt) % CURVE_ORDER
+  if (priavteKeyInt == 0):
+    return None
+  secret = (b'\0'*32 + int_to_string(priavteKeyInt))[-32:]
+  return secret, Ir
         
     
 
@@ -28,25 +52,33 @@ def childKeyDerivation(parentPriavteKey, parnetChaincode, index):
 # Calculate I = HMAC-SHA512(Key = "Bitcoin seed", Data = S)
 # Split I into two 32-byte sequences, IL and IR.
 # Use parse256(IL) as master secret key, and IR as master chain code.
-def generateMainKey(magicWord):
-    seed = hashlib.sha256(magicWord).digest()
-    key = b"Bitcoin seed"
-    i = hmacsha512(seed, key)
-    return i
+def generateMainKey(entropy):
+  seed = entropy
+  # seed = hashlib.sha256(entropy).digest()
+  key = b"Bitcoin seed"
+  i = hmacsha512(seed, key)
+  return i
 
 def hmacsha512(text, key):
-    sign = hmac.new(key, text, digestmod=hashlib.sha512)
-    digest = sign.digest()
-    privateKey = digest[:32]
-    chainCode = digest[32:]
-    return privateKey, chainCode
+  result = hmac.new(key, text, digestmod=hashlib.sha512).digest()
+  return (result[:32], result[32:])
+
+
+  
+def ser32(integer):
+  return struct.pack(">L", integer)
 
 def main():
-    testSeed = bytes.fromhex("000102030405060708090a0b0c0d0e0f")
-    root = bytes.fromhex("17231195")
-    key = generateMainKey(testSeed)
-    print(key[0].hex())
-    print(key[1].hex())
+  testSeed = bytes.fromhex("fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542")
+  # root = bytes.fromhex("17231195")
+  key = generateMainKey(testSeed)
+  masterPrivateKey = key[0]
+  masterChainCode = key[1]
+  masterPublicKey = privateToPublic(masterPrivateKey)
+  key0 = childKeyDerivationPrivate(masterPrivateKey, masterChainCode, 2147483647+BIP32_HARDEN)
+  print(key0[0].hex())
+  print(privateToPublic(key0[0]).hex())
+  print(key0[1].hex())
 
 
     
@@ -54,5 +86,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
-
+  main()
