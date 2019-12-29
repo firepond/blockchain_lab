eef852ca30f3f0f45eead7d090ada5c562e20602
firepond <915644848@qq.com>
2019-12-29 14:30:51 +0800

    Add .gitignore, add bitcoin wif

--- testnet/bip32.py
+++ testnet/bip32.py
@@ -3,6 +3,7 @@ import hmac
 import struct
 import ecdsa
 
+
 from secp256k1 import PrivateKey
 from binascii import hexlify, unhexlify
 from ecdsa.ecdsa import int_to_string, string_to_int
@@ -10,81 +11,65 @@ from ecdsa.curves import SECP256k1
 from ecdsa.numbertheory import square_root_mod_prime as sqrt_mod
 
 MIN_ENTROPY_LEN = 128        # bits
-BIP32_HARDEN    = 0x80000000 # choose from hardened set of child keys
-CURVE_GEN       = ecdsa.ecdsa.generator_secp256k1
-CURVE_ORDER     = CURVE_GEN.order()
-FIELD_ORDER     = SECP256k1.curve.p()
-INFINITY        = ecdsa.ellipticcurve.INFINITY
+BIP32_HARDEN = 0x80000000  # choose from hardened set of child keys
+CURVE_GEN = ecdsa.ecdsa.generator_secp256k1
+CURVE_ORDER = CURVE_GEN.order()
+FIELD_ORDER = SECP256k1.curve.p()
+INFINITY = ecdsa.ellipticcurve.INFINITY
 
 
-def privateToPublic(privateKey):
-  if(type(privateKey) == str):
-    privateKey = unhexlify(privateKey)
-  privkey = PrivateKey(privateKey)
-  compressed = privkey.pubkey.serialize()
-  # uncompressed = hexlify(privkey.pubkey.serialize(compressed=False)).decode('ascii')
-  # print(compressed)
-  # print(uncompressed)
-  return compressed
+def private_to_public(privateKey):
+    if(type(privateKey) == str):
+        privateKey = unhexlify(privateKey)
+    privkey = PrivateKey(privateKey)
+    compressed = privkey.pubkey.serialize()
+    uncompressed = hexlify(privkey.pubkey.serialize(
+        compressed=False)).decode('ascii')
+    # print(compressed)
+    # print(uncompressed)
+    return compressed
 
 
-def childKeyDerivationPrivate(masterPrivateKey, masterChaincode, index):
-  i_str = ser32(index)
-  if index & BIP32_HARDEN:
-    data = bytes.fromhex("00") + masterPrivateKey + bytes(i_str)
-  else:
-    data = privateToPublic(masterPrivateKey) + bytes(i_str)
-  (Il, Ir) = hmacsha512(data, masterChaincode)
-  ilInt =  string_to_int(Il)
-  if ilInt > CURVE_ORDER:
-    return None
-  privateInt = string_to_int(masterPrivateKey)
-  priavteKeyInt = (ilInt + privateInt) % CURVE_ORDER
-  if (priavteKeyInt == 0):
-    return None
-  secret = (b'\0'*32 + int_to_string(priavteKeyInt))[-32:]
-  return secret, Ir
-        
-    
+def child_key_derivation_from_private(master_private_key, master_chaincode, index):
+    i_str = ser32(index)
+    if index & BIP32_HARDEN:
+        data = bytes.fromhex("00") + master_private_key + bytes(i_str)
+    else:
+        data = private_to_public(master_private_key) + bytes(i_str)
+    (Il, Ir) = hmacsha512(data, master_chaincode)
+    ilInt = string_to_int(Il)
+    if ilInt > CURVE_ORDER:
+        return None
+    privateInt = string_to_int(master_private_key)
+    priavte_key_int = (ilInt + privateInt) % CURVE_ORDER
+    if (priavte_key_int == 0):
+        return None
+    secret = (b'\0'*32 + int_to_string(priavte_key_int))[-32:]
+    print("child key:" + str(index))
+    print("private key:" + secret.hex())
+    print("wif priavte key:" + importFormat.privatekey_to_wif(secret).hex())
+    publicKey = private_to_public(secret)
+    print("public key:" + publicKey.hex())
+    print("address:" + importFormat.publickey_to_address(publicKey).hex())
+    return secret, Ir
+
 
 # Generate a seed byte sequence S of a chosen length (between 128 and 512 bits; 256 bits is advised) from a (P)RNG.
 # Calculate I = HMAC-SHA512(Key = "Bitcoin seed", Data = S)
 # Split I into two 32-byte sequences, IL and IR.
 # Use parse256(IL) as master secret key, and IR as master chain code.
-def generateMainKey(entropy):
-  seed = entropy
-  # seed = hashlib.sha256(entropy).digest()
-  key = b"Bitcoin seed"
-  i = hmacsha512(seed, key)
-  return i
+def generate_main_key(entropy):
+    key = b"Bitcoin seed"
+    i = hmacsha512(entropy, key)
+    return i
+
 
 def hmacsha512(text, key):
-  result = hmac.new(key, text, digestmod=hashlib.sha512).digest()
-  return (result[:32], result[32:])
+    result = hmac.new(key, text, digestmod=hashlib.sha512).digest()
+    return (result[:32], result[32:])
 
 
-  
 def ser32(integer):
-  return struct.pack(">L", integer)
-
-def main():
-  # testSeed = bytes.fromhex("fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542")
-  testSeed =  hashlib.sha256(bytes(1723119)).digest()
-  # root = bytes.fromhex("17231195")
-  (masterPrivateKey, masterChainCode) = generateMainKey(testSeed)
-  
-  
-
-  masterPublicKey = privateToPublic(masterPrivateKey)
-  key0 = childKeyDerivationPrivate(masterPrivateKey, masterChainCode, 2147483647+BIP32_HARDEN)
-  print(key0[0].hex())
-  print(privateToPublic(key0[0]).hex())
-  print(key0[1].hex())
+    return struct.pack(">L", integer)
 
 
-    
-    
-
-
-if __name__ == "__main__":
-  main()
--- testnet/main.py
+++ testnet/main.py
@@ -1,34 +1,100 @@
 #!/usr/bin/python # -*- coding: UTF-8 -*-
 import base58
 import hashlib
-import common
 import bip32
 
-def publickey_to_address(public_key):
-  versionbytes = "6f"  
-  key1 = common.bin_ripemd160(common.bin_sha256(public_key))
-  # key1 = common.bin_ripemd160(public_key)
-  key2 = common.bin_sha256(common.bin_sha256(versionbytes+key1))
-  check = key2[0:8]
-  key3 = versionbytes + key1 + check
-  result = base58.b58encode_int(int(key3, 16))
-  return result
 
-def base58encode(text):
-  return base58.b58encode_int(int(text.hex(), 16))
+def bin_ripemd160(instr):
+    res = hashlib.new("ripemd160")
+    res.update(instr)
+    return res.digest()
+
+
+def bin_sha256(instr):
+    return hashlib.sha256(instr).digest()
+
+
+def privatekey_to_wif(privatekey):
+    if(type(privatekey) == str):
+        privatekey = bytes.fromhex(privatekey)
+    privatekey_with_prefix = bytes.fromhex("80") + privatekey
+    hash256 = bin_sha256(bin_sha256(privatekey_with_prefix))
+
+    check = hash256[0:4]
+    key3 = (privatekey_with_prefix + check).hex()
+    result = base58.b58encode_int(int(key3, 16))
+    return result
+
+
+def publickey_to_address(public_key):
+    if(type(public_key) == str):
+        public_key = bytes.fromhex(public_key)
+    version_bytes = bytes.fromhex("6f")
+    h160pk = bin_ripemd160(bin_sha256(public_key))
+    hash256 = bin_sha256(bin_sha256(version_bytes + h160pk))
+    check = hash256[0:4]
+    key3 = (version_bytes + h160pk + check).hex()
+    result = base58.b58encode_int(int(key3, 16))
+    return result
+
+
+def base58_encode(text):
+    return base58.b58encode_int(int(text.hex(), 16))
+
+
+def address_test():
+    pk_list = ['02b1ebcdbac723f7444fdfb8e83b13bd14fe679c59673a519df6a1038c07b719c6',
+               '036e69a3e7c303935403d5b96c47b7c4fa8a80ca569735284a91d930f0f49afa86']
+    for pk in pk_list:
+        print(publickey_to_address(pk))
+
+
+def wif_test():
+    test_seed = hashlib.sha256(bytes(17231195)).digest()
+    print("private key:" + test_seed.hex())
+    wif = privatekey_to_wif(test_seed)
+    print("wif:" + str(wif))
+
+
+def bip32test():
+    testSeed = hashlib.sha256(bytes(1723119)).digest()
+    # root = bytes.fromhex("17231195")
+    (master_private_key, master_chainCode) = generate_main_key(testSeed)
+
+    masterPublicKey = private_to_public(master_private_key)
+    key0 = child_key_derivation_from_private(
+        master_private_key, master_chainCode, 1)
+    # print("private key:" + key0[0].hex())
+    # print("public key:" + private_to_public(key0[0]).hex())
+    # print(key0[1].hex())
+    key1 = child_key_derivation_from_private(
+        master_private_key, master_chainCode, 2)
+    key1 = child_key_derivation_from_private(
+        master_private_key, master_chainCode, 3)
+    key1 = child_key_derivation_from_private(
+        master_private_key, master_chainCode, 4)
+
 
 def main():
-#   testSeed = hashlib.sha256(bytes(1723119)).digest()
-  testSeed =  bytes.fromhex("000102030405060708090a0b0c0d0e0f")
-  (masterPrivateKey, masterChainCode) = bip32.generateMainKey(testSeed)
-  masterPublicKey = bip32.privateToPublic(masterPrivateKey)
-  print("pvt, hex:", masterPrivateKey.hex())
-  print("pvt, ext:", base58encode(masterPrivateKey))
+    # test_seed = bytes.fromhex("000102030405060708090a0b0c0d0e0f")
+    # (masterPrivateKey, masterChainCode) = bip32.generateMainKey(test_seed)
+    # masterPublicKey = bip32.privateToPublic(masterPrivateKey)
+    # print("pvt, hex:", masterPrivateKey.hex())
+    # print("pvt, ext:", base58_encode(masterPrivateKey))
 
-  key0 = bip32.childKeyDerivationPrivate(masterPrivateKey, masterChainCode, 2147483647+bip32.BIP32_HARDEN)
-  print(key0[0].hex())
-  print(bip32.privateToPublic(key0[0]).hex())
-  print(key0[1].hex())
+    # key0 = bip32.childKeyDerivationPrivate(
+    #     masterPrivateKey, masterChainCode, 2147483647+bip32.BIP32_HARDEN)
+    # print(key0[0].hex())
+    # print(bip32.privateToPublic(key0[0]).hex())
+    # print(key0[1].hex())
+
+    # print("private key:" + test_seed.hex())
+    # public_key = bip32.privateToPublic(test_seed)
+    # print("public key:" + public_key.hex())
+    # print(publickey_to_address(public_key))
+    address_test()
+    wif_test()
+
 
 if __name__ == "__main__":
-  main()
+    main()
